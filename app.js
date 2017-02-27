5ce7f89e53edc79e3326ae964f4fae07e2c15ef0
Alison Stuart <hax@Alisons-MacBook-Air.local>
2017-02-27 13:19:24 -0500

    homepage renders

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const app = express();
+//require routes and set to variables 
+const auth = require('./routes/auth');
+const index = require('./routes/index');
+const user = require('./routes/user');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -18,6 +22,12 @@ app.use(bodyParser.urlencoded({
 }));
 // add new express-session and passport middleware here
 app.use(express.static(path.join(__dirname, 'public')));
+//----------------------------------------------
+//tell app.js which urls should lead to my routes
+app.use('/', index);
+app.use('/login', auth);
+app.use('/user', user);
+//----------------------------------------------
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('auth/index');
+    res.render('index');
 });
 module.exports = router;
